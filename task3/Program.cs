2f46e952396af1078bf75926e29de429ca14d3c2
Igor <guselnikov.uniikm@yandex.ru>
2022-07-24 13:40:28 +0500

    third task last version

--- task3/Program.cs
+++ task3/Program.cs
@@ -25,54 +25,68 @@ int rowsSecondArray = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов второй матрицы: ");
 int coloumnsSecondArray = Convert.ToInt32(Console.ReadLine());
 
-int[,] firstArray = new int[rowsFirstArray, columnsFirstArray];
-int[,] secondArray = new int[rowsSecondArray, coloumnsSecondArray];
-int[,] multipliedArray = new int[rowsFirstArray, columnsFirstArray];
+int[,] GetArray(int rows, int coloumns, int min, int max)
+{
+    int[,] matrix = new int [rows,coloumns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
+
+
+int[,] MultipliedArray(int[,] firstArray, int[,] secondArray)
+{
+    int[,] multipliedArray = new int [firstArray.GetLength(0),firstArray.GetLength(1)];
+    for (int i = 0; i < firstArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < firstArray.GetLength(1); j++)
+        {
+            multipliedArray[i, j] = firstArray[i, j] * secondArray[i, j];
+        }
+    }
+    return  multipliedArray;
+}
+
 
 if (rowsFirstArray == rowsSecondArray && columnsFirstArray == coloumnsSecondArray)
 {
-    Console.WriteLine();
+    int[,] firstArray = GetArray(rowsFirstArray, columnsFirstArray, 0, 15);
     Console.WriteLine();
     Console.WriteLine("Первая матрица");
     Console.WriteLine();
-    for (int i = 0; i < rowsFirstArray; i++)
-    {
-        for (int j = 0; j < columnsFirstArray; j++)
-        {
-            firstArray[i, j] = new Random().Next(0, 10);
-            Console.Write(firstArray[i, j] + "\t");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
+    PrintArray(firstArray);
+    int[,] secondArray = GetArray(rowsSecondArray, coloumnsSecondArray, 0, 15);
     Console.WriteLine();
     Console.WriteLine("Вторая матрица");
     Console.WriteLine();
-    for (int i = 0; i < rowsSecondArray; i++)
-    {
-        for (int j = 0; j < coloumnsSecondArray; j++)
-        {
-            secondArray[i, j] = new Random().Next(0, 10);
-            Console.Write(secondArray[i, j] + "\t");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
+    PrintArray(secondArray);
+    int [,] multipliedArray = MultipliedArray (firstArray, secondArray);
     Console.WriteLine();
     Console.WriteLine("Перемноженная матрица");
     Console.WriteLine();
-    for (int i = 0; i < rowsSecondArray; i++)
-    {
-        for (int j = 0; j < coloumnsSecondArray; j++)
-        {
-            multipliedArray[i, j] = firstArray[i,j] * secondArray[i,j];
-            Console.Write(multipliedArray[i, j] + "\t");
-        }
-        Console.WriteLine();
-    }
-
+    PrintArray(multipliedArray);
 }
 else
 {
     Console.WriteLine("Матрицы с веденными размерами перемножить нельзя!");
 }
+
